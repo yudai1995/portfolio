96b0d1828bdff668ea0d2a099879d798ce870ced
yudaiy <y.yamamoto991103@gmail.com>
2022-09-03 09:11:48 +0900

    fix meta for lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,25 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang='ja'>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <meta charSet='utf-8' />
+          <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+          <meta
+            name='viewport'
+            content='initial-scale=1.0, width=device-width'
+          />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='true'
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&family=Roboto:wght@300;700&family=Zen+Kaku+Gothic+New:wght@400;700&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&family=Roboto:wght@300;700&family=Zen+Kaku+Gothic+New:wght@400;700&display=swap'
+            rel='stylesheet'
           />
         </Head>
         <body>
