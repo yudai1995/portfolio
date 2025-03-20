cf283a4c55b72c96ae3c74bb4c5d60eebbf7f1c2
yamamoto <y.yamamoto991103@gmail.com>
2025-03-21 04:16:31 +0900

    add background particle

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
                     <link
                         rel='preconnect'
                         href='https://fonts.gstatic.com'
-                        crossOrigin='true'
+                        crossOrigin='anonymous'
                     />
                     <link
                         href='https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&family=Noto+Sans+JP:wght@300;400;500;600;700&family=Roboto:wght@300;700&family=Zen+Kaku+Gothic+New:wght@300;400;500;700&display=swap'
